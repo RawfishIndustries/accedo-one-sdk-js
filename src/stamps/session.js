ddbbefe7d7831834f37cde73ab1b41f1274e9494
elia_camposilvan <elia.camposilvan@rawfish.it>
2019-04-30 18:04:52 +0200

    linting session stamp

--- src/stamps/session.js
+++ src/stamps/session.js
@@ -98,7 +98,7 @@ const stamp = stampit({
               /*
                * refreshing the session only if `getSessionPromise` resolves to
                * the same value as before. This is necessary to avoid duplicating
-               * session requests while performing multiple concurrent data requests.
+               * session requests while performing multiple concurrent data fetching.
                */
               this.setSessionPromise(null);
               this.config.sessionKey = null;
